c9b5aa54309de122265e56bb653ce7e4a0392c08
Mugtaba <M-Subahi@hotmail.com>
2018-01-22 18:29:19 +0000

    Refactored Validation class and added comments

--- app/models/Validation.php
+++ app/models/Validation.php
@@ -2,75 +2,110 @@
 class Validation
 {
 
-  function validate($values, $rules)
+  private $values; // format: assoc array
+  private $rules; // $rules format: [ "name" => "required|min:3|max:3"]
+  private $errors = [];
+  private $valid = true;
+
+  // values to be looped through
+  private $splitFullRule; // equals array from "required|min:3|max:3" split at "|"
+  private $ruleName; // equals (currently looped) key from $rules;
+  private $ruleValue; // equals $values[$ruleName]
+  private $subRuleName; // equals "min" from "min:3"
+  private $subRuleOption; // equals "3" from "min:3"
+
+
+  // takes in array of data ($value) and rules to compare the data against
+  // returns if data is valid or not with errors where failed
+  function validate($values, $rules) : array
   {
-    // values format: assoc array
-    // $rules format:
-    // $rules = [ "name" => "required|min:3|max:3"]
+    $this->values = $values;
+    $this->rules = $rules;
 
-    $errors = [];
-    $valid = true;
+    // loops through each value (string) in $values array
+    foreach ($rules as $ruleName => $rule) {
+      $this->ruleName = $ruleName;
 
-    // loop through rules passed
-    foreach ($rules as $keyName => $rule) {
-      $splitFullRule = explode("|", $rule); // make numbered array of single rules by splitting value of key  at "|"
-      $errors[$keyName] = []; // adding key to $errors array and setting value to empty array
+      $this->splitFullRule = explode("|", $rule); // make array of sub rules by splitting value of key  at "|"
+      $this->errors[$ruleName] = []; // adding key to $errors array and setting value to empty array
 
-      foreach ($splitFullRule as $singleRule) {
-        $splitSingleRule = explode(":", $singleRule);// make numbered array by splitting single rule at ":"
-
-        $key = $values[$keyName];
-        $ruleName = $splitSingleRule[0]; // will be 'min' from min:3
-        if (count($splitSingleRule) > 1) {
-          $ruleOption = $splitSingleRule[1]; // will be '3' from min:3
-        }
-
-        switch ($ruleName) {
-          case 'required':
-            if (!isset($key) || empty($key) || strlen($key) === 0) {
-              $errors[$keyName][] = "field is required"; // appends new element to $errors array
-            }
-            break;
-
-          case 'email':
-            if (!filter_var($key, FILTER_VALIDATE_EMAIL)) {
-              $errors[$keyName][] = "$keyName must be in email format";
-            }
-            break;
-
-          case 'min':
-            if (strlen($key) < $ruleOption) {
-              $errors[$keyName][] = "$keyName must be at least $ruleOption characters";
-            }
-            break;
-
-          case 'max':
-            if (strlen($key) > $ruleOption) {
-              $errors[$keyName][] = "$keyName must be less than $ruleOption characters";
-            }
-            break;
-
-          case 'matches':
-            if ($key !== $values[$ruleOption]) {
-              $errors[$keyName][] = "$ruleOption does not match";
-            }
-            break;
-        }
-      }
-
-      // Check if any errors exist
-      foreach ($errors as $key => $value) {
-        if (count($value) > 0) {
-          $valid = false;
-        }
-      }
-
+      $this->subRuleSplitter();
+      $this->errorChecking();
     }
 
-    return [$valid, $errors];
+    return [
+      $this->valid,
+      $this->errors
+    ];
   }
 
 
+  function subRuleSplitter()
+  {
+    // loop through each sub rule (string) 
+    foreach ($this->splitFullRule as $singleRule) {
+      $subRule = explode(":", $singleRule);// make array from sub rules by splitting single rule at ":"
+
+      // setting $ruleValue to value of rule (from origin data aray) currently being looped
+      $this->ruleValue = $this->values[$this->ruleName];
+      $this->subRuleName = $subRule[0]; // equals 'min' from min:3
+
+      if (count($subRule) > 1) { // if $subRule has ":", count wil be over 1
+        $this->subRuleOption = $subRule[1]; // equals '3' from min:3
+      }
+
+      $this->subRuleCriteria();
+    }
+  }
+
+
+  // runes each sub rule name against its own set rules
+  function subRuleCriteria()
+  {
+    switch ($this->subRuleName) {
+      case 'required':
+        if (!isset($this->ruleValue) || empty($this->ruleValue) || strlen($this->ruleValue) === 0) {
+          $this->errors[$this->ruleName][] = "field is required"; // appends new element to $errors array
+        }
+        break;
+
+      case 'email':
+        if (!filter_var($this->ruleValue, FILTER_VALIDATE_EMAIL)) {
+          $this->errors[$this->ruleName][] = "$this->ruleName must be in email format";
+        }
+        break;
+
+      case 'min':
+        if (strlen($this->ruleValue) < $this->subRuleOption) {
+          $this->errors[$this->ruleName][] = "$this->ruleName must be at least $this->subRuleOption characters";
+        }
+        break;
+
+      case 'max':
+        if (strlen($this->ruleValue) > $this->subRuleOption) {
+          $this->errors[$this->ruleName][] = "$this->ruleName must be less than $this->subRuleOption characters";
+        }
+        break;
+
+      case 'matches':
+        if ($this->ruleValue !== $this->values[$this->subRuleOption]) {
+          $this->errors[$this->ruleName][] = "$this->subRuleOption does not match";
+        }
+        break;
+    }
+  }
+
+
+  // loops through $errors array counting each value
+  // if count is greater than 0 (any errors exist), data is not valid  
+  function errorChecking()
+  {
+    foreach ($this->errors as $key => $value) {
+      if (count($value) > 0) {
+        $this->valid = false;
+      }
+    }
+  }
 
 }
 
---
+++ app/models/ValidationBackup.php
@@ -0,0 +1,77 @@
+<?php
+class Validation
+{
+
+  function validate(array $values, array $rules) : array
+  {
+    // values format: assoc array
+    // $rules format:
+    // $rules = [ "name" => "required|min:3|max:3"]
+
+    $errors = [];
+    $valid = true;
+
+    // loop through rules passed
+    foreach ($rules as $keyName => $rule) {
+      $splitFullRule = explode("|", $rule); // make numbered array of single rules by splitting value of key  at "|"
+      $errors[$keyName] = []; // adding key to $errors array and setting value to empty array
+
+      foreach ($splitFullRule as $singleRule) {
+        $splitSingleRule = explode(":", $singleRule);// make numbered array by splitting single rule at ":"
+
+        $key = $values[$keyName];
+        $ruleName = $splitSingleRule[0]; // will be 'min' from min:3
+        if (count($splitSingleRule) > 1) {
+          $ruleOption = $splitSingleRule[1]; // will be '3' from min:3
+        }
+
+        switch ($ruleName) {
+          case 'required':
+            if (!isset($key) || empty($key) || strlen($key) === 0) {
+              $errors[$keyName][] = "field is required"; // appends new element to $errors array
+            }
+            break;
+
+          case 'email':
+            if (!filter_var($key, FILTER_VALIDATE_EMAIL)) {
+              $errors[$keyName][] = "$keyName must be in email format";
+            }
+            break;
+
+          case 'min':
+            if (strlen($key) < $ruleOption) {
+              $errors[$keyName][] = "$keyName must be at least $ruleOption characters";
+            }
+            break;
+
+          case 'max':
+            if (strlen($key) > $ruleOption) {
+              $errors[$keyName][] = "$keyName must be less than $ruleOption characters";
+            }
+            break;
+
+          case 'matches':
+            if ($key !== $values[$ruleOption]) {
+              $errors[$keyName][] = "$ruleOption does not match";
+            }
+            break;
+        }
+      }
+
+      // Check if any errors exist
+      foreach ($errors as $key => $value) {
+        if (count($value) > 0) {
+          $valid = false;
+        }
+      }
+
+    }
+
+    return [$valid, $errors];
+  }
+
+
+
+}
+
+?>
