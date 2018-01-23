dbfd87e456424f4cf74128cbe5ca6756ffa277fc
Mugtaba <M-Subahi@hotmail.com>
2018-01-23 21:12:00 +0000

    test

--- app/controllers/Home/home.php
+++ app/controllers/Home/home.php
@@ -5,7 +5,7 @@ class Home extends Controller
   function __construct()
   {
 
-    $password = "bbbbbs";
+    $password = "bbbbbsa";
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $isPassword = password_verify('abc', $hashedPassword);
