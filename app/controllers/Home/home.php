2b4cfd1f83cd010c774590ae7c67d45d799f07d9
Mugtaba <M-Subahi@hotmail.com>
2018-01-23 21:22:09 +0000

    test branch commit 1

--- app/controllers/Home/home.php
+++ app/controllers/Home/home.php
@@ -5,7 +5,7 @@ class Home extends Controller
   function __construct()
   {
 
-    $password = "Editing BEFIRE creating new branch";
+    $password = "Editing BEFIRE creating new branch COMMIT1";
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     $isPassword = password_verify('abc', $hashedPassword);
